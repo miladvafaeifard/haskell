d62f3397d667238632698991d8aa1620a8b11b9f
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2024-10-03 16:30:36 +0200

    Complete Hw2

--- homework2/LogAnalysis.hs
+++ homework2/LogAnalysis.hs
@@ -3,20 +3,24 @@
 module LogAnalysis where 
     
 import Log as L
-
-parseInt :: String -> Int
-parseInt s = read s :: Int
+import Util as Util
 
 parseMessage :: String -> LogMessage
 parseMessage s =
   case s of 
-    ('I':_) -> L.LogMessage Info (parseInt timestamp) (unwords content)
-    ('W':_) -> L.LogMessage Warning (parseInt timestamp) (unwords content)
-    ('E':_) -> L.LogMessage (Error (parseInt e_severity)) (parseInt e_timestamp) (unwords e_content)
+    ('I':_) -> L.LogMessage Info (Util.parseInt timestamp) (unwords content)
+    ('W':_) -> L.LogMessage Warning (Util.parseInt timestamp) (unwords content)
+    ('E':_) -> L.LogMessage (Error (Util.parseInt e_severity)) (Util.parseInt e_timestamp) (unwords e_content)
     _ -> Unknown s
     where
       (_:timestamp:content) = words s
       (_:e_severity:e_timestamp:e_content) = words s
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong [] = []
+whatWentWrong ((LogMessage (Error severity) timestamp content):xs)
+  | severity > 50 = content : whatWentWrong xs
+whatWentWrong (_ : xs) = whatWentWrong xs
 
 -- Function to parse the entire log file
 parse :: String -> [LogMessage]
---
+++ homework2/Util.hs
@@ -0,0 +1,6 @@
+-- {-# OPTIONS_GHC-Wall #-}
+
+module Util where 
+
+parseInt :: String -> Int
+parseInt s = read s :: Int
